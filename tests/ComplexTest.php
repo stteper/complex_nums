05a87f30e52bbe560335525b30a9cc3b126fb5eb
Alex Stepanov <stteper@gmail.com>
2021-09-10 18:07:27 +0300

    Add test class

---
+++ tests/ComplexTest.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: comp
+ * Date: 10.09.2021
+ * Time: 18:03
+ */
+namespace MathTest;
+use PHPUnit\Framework\TestCase;
+
+class ComplexTest extends TestCase
+{
+
+}
